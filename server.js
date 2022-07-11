7e5e3cab69086431700ee33d11e7f834c415a7c3
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-07-11 23:13:24 +0530

    Add existing file

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://shopp-e.netlify.app/",
     // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     // allowedHeaders: ['Content-Type', 'Authorization']
 }));
